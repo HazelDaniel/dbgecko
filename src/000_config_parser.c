9e7b9539bc7cd4268da5630bf58347354f697829
HazelDaniel <hazeldaniel856@gmail.com>
2025-11-28 22:05:10 +0100

    feat: added platform version - config compatibility check

--- src/000_config_parser.c
+++ src/000_config_parser.c
@@ -196,5 +196,9 @@ void validate_app_config(AppConfig_t *cfg, StackError_t **err) {
   message = "temporary runtime directory not provided!";
   VALIDATE_COND_WITH_MESSAGE((!cfg->runtime->temp_dir[0]), message);
 
+  message = "incompatible platform version";
+  VALIDATE_COND_WITH_MESSAGE((cfg->platform->version < CURRENT_PLATFORM_VERSION - VERSION_SUPPORT_RANGE ||\
+    cfg->platform->version > CURRENT_PLATFORM_VERSION + VERSION_SUPPORT_RANGE), message);
+
   #undef VALIDATE_COND_WITH_MESSAGE
 }
