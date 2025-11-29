3f4cb15384f997c9e15545078907ec3239a5b4c3
HazelDaniel <hazeldaniel856@gmail.com>
2025-11-29 08:21:31 +0100

    chore: change install location for platform binary

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
-
-
+#!/usr/bin/env bash
 
 make -C build
+make install -C build
 
-sudo make install -C build
+homedir="$HOME"
+
+if ! grep -Pi "#dbgecko" ~/.zshrc ; then
+	echo "export PATH=\"\$PATH:$(cd `dirname $0` && pwd)/bin\" #dbgecko" >> "$homedir/.zshrc"
+
+	if type -P zsh > /dev/null 2>&1; then # zshell support
+		echo "source $homedir/.zshrc" | zsh
+	fi
+fi
+
